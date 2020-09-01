4d8a5af7c46c5eb2c8234b71c33f8a803f028e60
Mikhail Tikhonov <46346390+MikhailTikhonovA@users.noreply.github.com>
2020-09-01 20:34:20 +0300

    Update BaseTest.java

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,9 +1,20 @@
 package tests;
 
 import com.codeborne.selenide.Selenide;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static com.codeborne.selenide.logevents.SelenideLogger.addListener;
 
 public class BaseTest {
+
+    @BeforeEach
+    public void setUp(){
+        addListener("AllureSelenide", new AllureSelenide().screenshots(true).savePageSource(true));
+    }
+
+
     @AfterEach
     public void tearDown(){
         Selenide.closeWindow();
